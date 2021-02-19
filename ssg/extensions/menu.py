6630390ded10980353bebe8c9af190220770ec46
Matthew Wright <mwright137@gmail.com>
2021-02-19 15:00:54 -0500

    module 3 fixed??

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -3,7 +3,7 @@ from ssg import hooks, parsers
 files = []
 
 
-hooks.register("collect_files")
+@hooks.register("collect_files")
 def collect_files(source, site_parsers):
   valid = lambda p: not isinstance(p, parsers.ResourceParser)
   for path in source.rglob("*"):
@@ -12,7 +12,7 @@ def collect_files(source, site_parsers):
         files.append(path)
 
 
-hooks.register("generate_menu")
+@hooks.register("generate_menu")
 def generate_menu(html, ext):
   template = '<li><a href="{}{}">{}</a></li>'
   menu_item = lambda name, ext: template.format(name, ext, name.title())
