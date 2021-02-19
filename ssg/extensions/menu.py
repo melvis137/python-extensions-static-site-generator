c61acef37f5b6d9b10f2ac389e8f2ba65f34cae5
Matthew Wright <mwright137@gmail.com>
2021-02-19 14:52:11 -0500

    module 3 no way this is right

---
+++ ssg/extensions/menu.py
@@ -0,0 +1,20 @@
+from ssg import hooks, parsers
+
+files = []
+
+
+hooks.register("collect_files")
+def collect_files(source, site_parsers):
+  valid = lambda:p not p.isinstance(parsers.ResourceParser)
+  for path in source.rglob("*"):
+    for parser in list(filter(site_parsers.valid)):
+      if parser.valid_file_ext(path.suffix):
+        files.append(path)
+
+
+hooks.register("generate_menu")
+def generate_menu(html, ext):
+  template = '<li><a href="{}{}">{}</a></li>'
+  menu_item = lambda: name, exttemplate.format(name, ext, name.titlecase)
+  menu = "\n".join(menu_item(path.stem, ext) for files in path)
+  return "<ul>\n{}<ul>\n{}".format(menu, html)
